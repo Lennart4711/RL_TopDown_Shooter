8c86138bd1a718dddcea53596d8cad9245a30b3d
Lennart4711 <lennart.randomthings@gmail.com>
2023-06-25 17:22:16 +0200

    Started to prepare for rl environment

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ import random
 pygame.init()
 
 from player import Player
+from utils import crossed_wall
 
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 400
+HEIGHT = 400
+RUNNING_SPEED = 2
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
-p1 = Player(np.array([WIDTH / 2, HEIGHT / 2]), 0)
+p1 = Player(np.array([WIDTH / 4, HEIGHT / 2]), 0)
+target = Player(np.array([WIDTH / 1.5, HEIGHT / 2]), 0)
 
 running = True
 
@@ -21,17 +25,8 @@ walls = [
     [np.array([WIDTH, 0]), np.array([WIDTH, HEIGHT])],
     [np.array([WIDTH, HEIGHT]), np.array([0, HEIGHT])],
     [np.array([0, HEIGHT]), np.array([0, 0])],
-    [np.array([WIDTH / 2, HEIGHT / 2]), np.array([WIDTH / 2, HEIGHT / 2 + 100])],
+    [np.array([WIDTH / 2, HEIGHT/4]), np.array([WIDTH / 2, HEIGHT / 1.5])],
 ]
-
-for _ in range(10):
-    x1 = random.randint(0, WIDTH)
-    y1 = random.randint(0, HEIGHT)
-    x2 = random.randint(0, WIDTH)
-    y2 = random.randint(0, HEIGHT)
-
-    walls.append([np.array([x1, y1]), np.array([x2, y2])])
-
 
 
 while running:
@@ -48,19 +43,41 @@ while running:
     if keys[pygame.K_d]:
         p1.rotate(0.1)
 
-    # if w or s is pressed, move player1
-    if keys[pygame.K_w]:
-        p1.move_forward(walls, 2)
+    # Rotate towards the mouse
+    mouse_pos = np.array(pygame.mouse.get_pos())
+    p1.rotate_towards(mouse_pos)
 
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_w]:
+        p1.move(walls, 0, -RUNNING_SPEED)
+    if keys[pygame.K_s]:
+        p1.move(walls, 0, RUNNING_SPEED)
+    if keys[pygame.K_a]:
+        p1.move(walls, -RUNNING_SPEED, 0)
+    if keys[pygame.K_d]:
+        p1.move(walls, RUNNING_SPEED, 0)
+
+    # if mouse is pressed, shoot
+    if pygame.mouse.get_pressed()[0]:
         p1.shoot()
 
     p1.move_bullets(walls, [])
 
     p1.draw(screen)
+    p1.cast_ray(screen, walls, -45)
+    p1.cast_ray(screen, walls, 45)#
     p1.draw_bullets(screen)
     for wall in walls:
         pygame.draw.line(screen, (255, 255, 255), wall[0], wall[1])
 
+    FOW = np.deg2rad(90)
+    # If the player can see the target, draw a line between them
+    if not any(crossed_wall(wall, [p1.pos, target.pos]) for wall in walls):
+        # Draw the line if the player can see the target inside a 90 degree angle
+        if p1.sees_player(target, 45):
+            pygame.draw.line(screen, (0, 255, 0), p1.pos, target.pos)
+            target.draw(screen)
+
+
     clock.tick(60)
     pygame.display.update()
+
--- utils.py
+++ utils.py
@@ -14,3 +14,28 @@ def crossed_wall(wall: np.ndarray, trajectory: np.ndarray):
     D = trajectory[1]
 
     return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
+
+
+def line_line_intersection(line1, line2):
+    x1, y1 = line1[0]  # Endpoint 1 of line 1
+    x2, y2 = line1[1]  # Endpoint 2 of line 1
+    x3, y3 = line2[0]  # Endpoint 1 of line 2
+    x4, y4 = line2[1]  # Endpoint 2 of line 2
+
+    # Calculate the denominator
+    denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
+
+    # If the denominator is zero, the lines are parallel or coincident
+    if denominator == 0:
+        return None
+
+    # Calculate the intersection point coordinates
+    intersection_x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / denominator
+    intersection_y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / denominator
+
+    # Check if the intersection point lies within the line segments
+    if (min(x1, x2) <= intersection_x <= max(x1, x2) and min(y1, y2) <= intersection_y <= max(y1, y2) and
+            min(x3, x4) <= intersection_x <= max(x3, x4) and min(y3, y4) <= intersection_y <= max(y3, y4)):
+        return intersection_x, intersection_y
+    else:
+        return None
